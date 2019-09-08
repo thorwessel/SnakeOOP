ed5c7f7e0ee709cf2c828c2a812da68903b8a10b
wesselexe <Ledigmand1>
2019-09-08 13:34:27 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = 'snakeOOP'
+
+enableFeaturePreview("GRADLE_METADATA")
---
+++ src/jvmMain/kotlin/main.kt
@@ -0,0 +1,5 @@
+import com.soywiz.korge.*
+
+supend fun main() = Korge {
+    solidRect(100, 100, Colors.RED)
+}
