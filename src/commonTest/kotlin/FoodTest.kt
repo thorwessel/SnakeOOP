f97c88b5ab400347803082de1b004c5d5b7d34c1
thowessel <thor.b.wessel@gmail.com>
2019-09-17 21:46:05 +0200

    Corrected the for fun test

--- src/commonTest/kotlin/FoodTest.kt
+++ src/commonTest/kotlin/FoodTest.kt
@@ -19,37 +19,42 @@ class FoodTest() {
     }
 
     @Test
-    fun `check if positions returned is truly random`() {
+    fun `check if positions returned is evenly distributed`() {
+        /* This test is just for fun, I wanted to try my luck at testing randomness. It turns out, that is rather
+        difficult, or at least require basic knowledge of math. Instead, I wanted to look into whether the positions
+        from the getNewFoodLocation() method actually was equally distributed. To do this, I make a list using another
+        random calculation, then add twice as many numbers from the getNewFoodLocation() method. Make a Map of the
+        integers to see how many of each was on the original list and then check whether each integer is within 10 %
+        of what a equal distribution of integers would be. Made sense, no? Good.
+         */
         val testFood = Food()
         val randomList = getRandomList(Random(69))
 
-        for (i in 1..10000) {
+        (1..10000).forEach {
             val positionsFromFood = testFood.getNewFoodLocation()
             randomList.add(positionsFromFood.xPosition)
             randomList.add(positionsFromFood.yPosition)
         }
 
         val sortedMapOfRandomValues = mutableMapOf<Int, Int>()
-        for (i in 0..15) {
-            sortedMapOfRandomValues[i] = 0
+        (0..15).forEach {
+            sortedMapOfRandomValues[it] = 0
         }
 
         randomList.forEach {
             sortedMapOfRandomValues[it] = sortedMapOfRandomValues.getValue(it) + 1
         }
 
-        println(sortedMapOfRandomValues)
-
-        var isItRandomQuestionmark = true
+        var isItRandomQuestionMark = true
         val expectedValue = randomList.size / 16
         val allowedUpperDeviation = expectedValue / 100 * 10
 
         sortedMapOfRandomValues.forEach {
-            if (it.key > expectedValue + allowedUpperDeviation || it.key < expectedValue - allowedUpperDeviation) {
-                isItRandomQuestionmark = false
+            if (it.value > expectedValue + allowedUpperDeviation || it.value < expectedValue - allowedUpperDeviation) {
+                isItRandomQuestionMark = false
             }
         }
 
-        assertEquals(true, isItRandomQuestionmark, "The numbers are not equally distributed?!?!")
+        assertEquals(true, isItRandomQuestionMark, "The numbers are not equally distributed?!?!")
     }
 }
