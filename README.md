950f59f1e4d2aea2ddd1bb83e2f4ffca4cb91eda
thowessel <thor.b.wessel@gmail.com>
2019-09-28 18:59:06 +0200

    Removed comments and updated readme

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 Simple snake game written in an OOP manner.
 The program uses Korge to render the graphics and to listen for keystrokes. Unfortunately, I did not manage to preserve a fully object-oriented programming style, while using Korge. Therefor, all the graphics related code and key listeners are present in the main.kt file and not in separate classes.
 
- The overall structure is rather simple, 3 classes are used:
+ The overall structure is rather simple, 5 classes are used:
+### Game
+The Game class is the top level class which interacts with all other classes, it provides methods to progress the game and return the score.
+
+### Graphics
+To render the graphics with Korge, a View object is created for each time-step/frame in the game. The Graphics class takes the positions of the Snake(s) and Food and return a View to be displayed.
+
 ### Snake
-Snake is where most of the heavy lifting is. The Snake class both contains the states of the snake which is used to render the snake in the game. The class also contains methods to add input from the user, check whether the snake has hit part of its body, add check if the snake has eaten the food of the game.
+Snake is where most of the heavy lifting is. The Snake class contains the states of the snake which is used to render the snake in the game.
+The class also contains methods to add input from the user, check whether the snake has hit part of its body, add check if the snake has eaten the food of the game.
 
 
 ### Movement
--- src/commonTest/kotlin/SnakeTest.kt
+++ src/commonTest/kotlin/SnakeTest.kt
@@ -13,138 +13,22 @@ class SnakeTest {
     @Test
     fun `When next state is calculated the last state of snake is removed`() {
         val snake = Snake()
-        val initialState = snake.resetInternalState()
-        val sizeOfSnake = (initialState.size - 1)
-        assert(snake.getNextSnake()[sizeOfSnake] != initialState[sizeOfSnake])
+        val initialStates = snake.resetInternalState()
+        val sizeOfSnake = (initialStates.size - 1)
+        val nextStates = snake.getNextSnake()
+        assert(nextStates[sizeOfSnake] != initialStates[sizeOfSnake])
     }
-
-
-    /*
-    @Test
-    fun `When direction of the snake is left, the next position will be x - 1`() {
-        val snake = Snake()
-        val initialState = snake.resetInternalState()
-
-        /*
-        Initial state = State(
-            xPosition = 8,
-            yPosition = 8,
-            length = 1,
-            nextDirections = mutableListOf(Direction.left))
-         */
-
-        initialState[0].nextDirections = mutableListOf(Direction.left)
-
-        assertEquals(expected = State(
-            xPosition = 7,
-            yPosition = 8,
-            length = 1,
-            nextDirections = mutableListOf(Direction.left)),
-
-            actual = snake.getNextSnake()[0],
-
-            message = "xPosition is not updated correctly on direction left"
-        )
-    }
-
-    @Test
-    fun `When direction of the snake is up, the next position will be y - 1`() {
-        val snake = Snake()
-        val initialState = snake.resetInternalState()
-
-        /*
-        Initial state = State(
-            xPosition = 8,
-            yPosition = 8,
-            length = 1,
-            nextDirections = mutableListOf(Direction.left))
-         */
-
-        initialState[0].nextDirections = mutableListOf(Direction.up)
-
-        assertEquals(expected = State(
-            xPosition = 8,
-            yPosition = 7,
-            length = 1,
-            nextDirections = mutableListOf(Direction.up)),
-
-            actual = snake.getNextSnake()[0],
-
-            message = "yPosition is not updated correctly on direction up"
-        )
-    }
-
-    @Test
-    fun `When direction of the snake is right, the next position will be x + 1`() {
-        val snake = Snake()
-        val initialState = snake.resetInternalState()
-
-        /*
-        Initial state = State(
-            xPosition = 8,
-            yPosition = 8,
-            length = 1,
-            nextDirections = mutableListOf(Direction.left))
-         */
-
-        initialState[0].nextDirections = mutableListOf(Direction.right)
-
-        assertEquals(expected = State(
-            xPosition = 9,
-            yPosition = 8,
-            length = 1,
-            nextDirections = mutableListOf(Direction.right)),
-
-            actual = snake.getNextSnake()[0],
-
-            message = "xPosition is not updated correctly on direction right"
-        )
-    }
-
-    @Test
-    fun `When direction of the snake is down, the next position will be y + 1`() {
-        val snake = Snake()
-        val initialState = snake.resetInternalState()
-
-        /*
-        Initial state = State(
-            xPosition = 8,
-            yPosition = 8)
-         */
-
-        initialState.nextDirections = mutableListOf(Direction.down)
-
-        assertEquals(expected = Position(
-            xPosition = 8,
-            yPosition = 9),
-
-            actual = snake.getNextSnake()[0],
-
-            message = "yPosition is not updated correctly on direction down"
-        )
-    }
-
-    @Test
-    fun `Check if last queued direction is correctly returned`() {
-        val snake = Snake()
-        snake.resetInternalState()
-
-        snake.addDirection(Direction.up)
-        snake.addDirection(Direction.left)
-        snake.addDirection(Direction.right)
-        snake.addDirection(Direction.down)
-
-        assertEquals(Direction.down, snake.getLastQueuedDirection(), "Last added direction was not returned")
-    }
-
-     */
-
 
     @Test
     fun `Check that snake self-collision is detected`() {
         val snake = Snake()
         snake.resetInternalState()
-        snake.length = 6
+        snake.addLength()
+        snake.addLength()
+        snake.addLength()
+        snake.addLength()
+        snake.addLength()
+        snake.addLength()
         snake.addDirection(Direction.Left)
         snake.getNextSnake()
         snake.addDirection(Direction.Up)
